de9849661af017d4e1058cf781931d1644d3c786
Guus Claessen <combatgup@hotmail.com>
2018-05-12 01:35:13 +0200

    Removed comments

--- src/app/leaflet/map/component/map/map.component.ts
+++ src/app/leaflet/map/component/map/map.component.ts
@@ -7,19 +7,8 @@ import { MapService } from '../../service/map.service';
 	styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-	// hideMap = false;
 	
 	constructor(private mapService: MapService) { 
-		// this.mapService.hide.subscribe(hide => {
-		// 	this.hideMap = hide;
-
-		// 	// setTimeout(() => {
-		// 	// 	if(!this.hideMap) {
-		// 	// 		this.mapService.resetView();
-		// 	// 		this.mapService.map.invalidateSize();
-		// 	// 	}
-		// 	// }, 0);
-		// });
 	}
 
 	ngOnInit() {
